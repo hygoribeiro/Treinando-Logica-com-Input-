9500addbe1a5c1c5998a5b927a931f60fd4a113e
hygor riberio <hygoribeiro77@gmail.com>
2023-02-08 04:36:55 -0300

    add comentarios

--- juros_De_Divida.js
+++ juros_De_Divida.js
@@ -1,15 +1,17 @@
 import entradaDAdos from "readline-sync";
 
 while(true){
+//entrada de dados do usuario 
 const valorDaDivida = entradaDAdos.question("informe o valo devido: ");
 
+//validação para não fazer calculo de 0
 if(valorDaDivida === "0"){
     console.log("o Valor da Divida Deve Ser Maior que 0 !!!");
     break
-}
+};
 
 const vencimento = entradaDAdos.question("infome quantos dias se passou des do dia do vencimento do boleto :");
-
+//saida de dados informado pelo usuario 
 console.log("Valor da Divida " + valorDaDivida);
 console.log("Dias de Atraso: " + vencimento);
 
@@ -27,6 +29,7 @@ function calculoDeJuros(){
         console.log ("Valor Total com o Juros: " + ( Number(jurosAbaixoDe15) + Number (valorDaDivida)));
     }
 };
+
 calculoDeJuros()
 
 break
