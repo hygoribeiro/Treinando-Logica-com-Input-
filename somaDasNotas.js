5882125654c0396e2ce9a36436d35b9225db1f3f
hygor riberio <hygoribeiro77@gmail.com>
2023-02-09 04:01:09 -0300

    simplificando com .length

--- somaDasNotas.js
+++ somaDasNotas.js
@@ -1,6 +1,5 @@
 const notas = [5,8,3,10];
-const totalDasNotas = notas.length;
 
 const SomaDasNotas = (notas[0] + notas [1] + notas[2] + notas [3]);
-const mediaDasNotas = (SomaDasNotas / totalDasNotas);
+const mediaDasNotas = (SomaDasNotas / notas.length);
 console.log("a media escolar desse aluno e de " + mediaDasNotas);
