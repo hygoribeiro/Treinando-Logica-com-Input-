bb4a4dc194bd0c5386d7a982ec26d7b52c72c130
hygor riberio <hygoribeiro77@gmail.com>
2023-02-11 21:31:13 -0300

    trinando array

---
+++ array.js
@@ -0,0 +1,15 @@
+let meses = [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setempro",
+    "outubro",
+    "novembro",
+    "dezenbro"
+];
+console.log (meses);
