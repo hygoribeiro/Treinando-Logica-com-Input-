be0a4847a3eafe23415c581bd833c29537f9ddb7
hygor riberio <hygoribeiro77@gmail.com>
2023-02-22 05:15:55 -0300

    arrumando espaços

--- taboada.js
+++ taboada.js
@@ -1,16 +1,20 @@
 import readline from "readline-sync";
+
 let contador = 0;
 
 for(let i = 0; i <= 100; i++){
     let numeroEscolido = readline.question("escola o numero quer vc quer ver a tabuada? ");
     let decimal = 0;
+
     while(contador <= 10){
         let multiplicacao = numeroEscolido * decimal;
         console.log(`${numeroEscolido} x ${decimal} = ${multiplicacao}`)
         decimal++
         contador++
     }
+
     let continuar = readline.question("desseja fazer outra multiplicação? (sim ou nao) ");
+
     if(continuar == "nao"){
         break
     }
