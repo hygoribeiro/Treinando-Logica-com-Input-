3e8f57c3a26fcfdf111f26d8b391fc3397ec8e9f
hygor riberio <hygoribeiro77@gmail.com>
2023-02-24 04:46:48 -0300

    deixando o codigo mais facil de ler

--- treinandoFunction.js
+++ treinandoFunction.js
@@ -9,15 +9,18 @@ const media = () => {
     let notas = alunos[contador];
     return(notas.nota1 + notas.nota2) /2;
 }
-
-while(contador < alunos.length){
+const passou = () => {
     if(media() >= 7 ){
         console.log(`esse alunos forão aprovados ${alunos[contador].nome} com mdeia ${media()}`)
     }else{
         console.log("")
         console.log(`esses alunos forão reprovados ${alunos[contador].nome} com media ${media()}`)
     }
-    
+
+}
+
+while(contador < alunos.length){
+    passou()
     contador ++
 }
 
