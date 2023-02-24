a5f6d7015d4cee7ca0317c15168a8314d341f29a
hygor riberio <hygoribeiro77@gmail.com>
2023-02-24 04:27:14 -0300

    frist commit

---
+++ treinandoFunction.js
@@ -0,0 +1,26 @@
+const alunos = [
+    {nome: "Hygor", nota1: 7, nota2: 9},
+    {nome: "Maria", nota1: 7, nota2: 10},
+    {nome: "Lorisvaldo", nota1: 4, nota2: 10},
+    {nome: "Joaquina", nota1: 10, nota2:3},
+];
+let contador = 0;
+
+while(contador < alunos.length){
+    let notas = alunos[contador];
+
+    const media = () => {
+        return(notas.nota1 + notas.nota2) /2;
+    }
+    if(media() >= 7 ){
+        console.log(`esse alunos forão aprovados ${alunos[contador].nome} com mdeia ${media()}`)
+    }else{
+        console.log("")
+        console.log(`esses alunos forão reprovados ${alunos[contador].nome} com media ${media()}`)
+    }
+    
+    contador ++
+}
+
+
+
